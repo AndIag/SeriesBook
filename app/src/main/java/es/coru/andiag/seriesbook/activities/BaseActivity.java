ed4d290c012947ea20ef3c603009f00818cbaaa9
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-02-18 22:25:47 +0100

    Themes feature done

---
+++ app/src/main/java/es/coru/andiag/seriesbook/activities/BaseActivity.java
@@ -0,0 +1,51 @@
+package es.coru.andiag.seriesbook.activities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import es.coru.andiag.seriesbook.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    public static final String TAG = "BaseActivity";
+    public static final String PREF_THEME_KEY = "theme_list";
+
+    public final static int THEME_DARK = 0;
+    public final static int THEME_LIGHT = 1;
+
+    public static int getTheme(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return Integer.valueOf(prefs.getString(PREF_THEME_KEY, "-1"));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        updateTheme();
+    }
+
+    private void updateTheme() { //Change theme to all activities that extends BaseActivity
+        if (getTheme(getApplicationContext()) == THEME_DARK) {
+            setTheme(R.style.AppTheme_Dark);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.darkPrimaryDark));
+            }
+            return;
+        }
+        if (getTheme(getApplicationContext()) == THEME_LIGHT) {
+            setTheme(R.style.AppTheme);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(getResources().getColor(R.color.lightPrimaryDark));
+            }
+            return;
+        }
+        setTheme(R.style.AppTheme);
+    }
+}
--- app/src/main/java/es/coru/andiag/seriesbook/activities/MainActivity.java
+++ app/src/main/java/es/coru/andiag/seriesbook/activities/MainActivity.java
@@ -1,19 +1,21 @@
-package es.coru.andiag.seriesbook;
+package es.coru.andiag.seriesbook.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+import es.coru.andiag.seriesbook.R;
+import es.coru.andiag.seriesbook.fragments.SettingsFragment;
+
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -66,9 +68,12 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_container, new SettingsFragment())
+                        .commit();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -80,17 +85,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        switch (id) {
 
         }
 
