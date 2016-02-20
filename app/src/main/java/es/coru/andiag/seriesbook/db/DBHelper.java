8faaae17bfe30bb0791ca001c9e650e0955696c3
Iago <Santos Domnguez>
2016-02-20 13:50:01 +0100

    Preparing handle categories features (Add Category to nav_drawer)

---
+++ app/src/main/java/es/coru/andiag/seriesbook/db/DBHelper.java
@@ -0,0 +1,37 @@
+package es.coru.andiag.seriesbook.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by iagoc on 06/02/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String CATEGORY_TABLE = "categories";
+    public static final String CATEGORY_ID = "_id";
+    public static final String CATEGORY_NAME = "name";
+    public static final String CATEGORY_COLOR = "color"; //Color int value
+    private final static String TAG = "DBHelper";
+    private static final String DATABASE_NAME = "sbai.sqlite";
+    private static final int VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + CATEGORY_TABLE + " ("
+                + CATEGORY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + CATEGORY_NAME + " TEXT NOT NULL UNIQUE, "
+                + CATEGORY_COLOR + " INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/es/coru/andiag/seriesbook/entities/Category.java
@@ -0,0 +1,64 @@
+package es.coru.andiag.seriesbook.entities;
+
+import android.graphics.Color;
+
+import java.io.Serializable;
+
+/**
+ * Created by iagoc on 20/02/2016.
+ */
+public class Category implements Serializable {
+
+    private long id;
+    private String name;
+    private int color;
+
+    public Category() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = Color.parseColor(color);
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id == category.id && name.equals(category.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
