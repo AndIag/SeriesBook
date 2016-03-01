28c7d65443560c7c05bf8de03e7aec9a3de1e503
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-03-01 22:03:33 +0100

    Basic series adapter added

---
+++ app/src/main/java/es/coru/andiag/seriesbook/adapter/SeriesAdapter.java
@@ -0,0 +1,27 @@
+package es.coru.andiag.seriesbook.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by iagoc on 22/02/2016.
+ */
+public class SeriesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final static String TAG = "SeriesAdapter";
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
